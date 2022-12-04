ef364c2cc97106df66d7ab124368fb604420d134
Guilherme Alvarenga Dias <guilhermeadias@tecnico.ulisboa.pt>
2022-12-04 04:12:25 +0000

    Button test

--- AudioAux.py
+++ AudioAux.py
@@ -13,43 +13,74 @@ LED_R = LED(25)
 LED_G = LED(23)
 LED_B = LED(24)
 
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz sampling rate
+chunk = 1024 # 2^12 samples for buffer
+record_secs = 3 # seconds to record
+dev_index = 1 # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav' # name of .wav file
+
+audio = pyaudio.PyAudio() # create pyaudio instantiation
+
 print("init")
+frames = []
 try:
     while True:
         if(BUT_1.is_active):
+            LED_G.on()
             time.sleep(0.5)
+            # create pyaudio stream
+            stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+            print("recording")
             while True:
-                LED_G.on()
-                print("RECOGNIZING")
-
-                r = sr.Recognizer()
-                with sr.Microphone() as source:
-                    print("Say something!")
-                    audio = r.listen(source)
-
-                # recognize speech using Google Speech Recognition
-                try:
-                    # for testing purposes, we're just using the default API key
-                    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                    # instead of `r.recognize_google(audio)`
-                    print(str(r.recognize_google(audio)))
-                except sr.UnknownValueError:
-                    print("Google Speech Recognition could not understand audio")
-                except sr.RequestError as e:
-                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-                try:
-                    comment_analysis = requests.get(f"http://3.88.45.53:8000/appForNlp/nlp_result?comentario={comment}&id_pessoa={0}", timeout=5)
-                except:
-                    print("Connection Failed")
-                    comment_analysis = "CONNECTION_FAILED"
-
+                data = stream.read(chunk, False)
+                frames.append(data)
                 if BUT_1.is_active:
-                    LED_G.off()
-                    time.sleep(0.5)
-                    print("2")
                     break
+            print("recording2")
+            stream.stop_stream()
+            stream.close()
+            wavefile = wave.open(wav_output_filename,'wb')
+            wavefile.setnchannels(chans)
+            wavefile.setsampwidth(audio.get_sample_size(form_1))
+            wavefile.setframerate(samp_rate)
+            wavefile.writeframes(b''.join(frames))
+            wavefile.close()
+
+            #Recognizing
+            audio_file = path.join(path.dirname(path.realpath(__file__)), "test1.wav")
+
+            r = sr.Recognizer()
+            with sr.AudioFile(audio_file) as source:
+                audio2 = r.record(source)  # read the entire audio file
+
+            # recognize speech using Google Speech Recognition
+            comment = str("NOT_RECOGNIZED_BY_ANY")
+
+            try:
+                comment = str(r.recognize_google(audio2, language = 'en-US'))
+                print(comment)
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio - Switching to Sphinx")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service - Switching to Sphinx; {0}".format(e))
+
+            os.remove("test1.wav")
+
+            try:
+                comment_analysis = requests.get(f"http://3.88.45.53:8000/appForNlp/nlp_result?comentario={comment}&id_pessoa={0}", timeout=5)
+            except:
+                print("Connection Failed")
+                comment_analysis = "CONNECTION_FAILED"
+
+            LED_G.off()
+            time.sleep(0.5)
+            print("2")
 
 except Exception as e: 
     print(e)
     print("orra")
+    audio.terminate()
