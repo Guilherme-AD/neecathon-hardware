02d07177bd652b5d0430dd923e083b58f4fca3f5
Guilherme Alvarenga Dias <guilhermeadias@tecnico.ulisboa.pt>
2022-12-04 00:21:23 +0000

    Update

--- audioTest.py
+++ audioTest.py
@@ -1,14 +1,5 @@
 import pyaudio
 
-#PyAudio
-audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024, input_device_index=0)
-frames = []
-
-#Recording
-data=stream.read(1024)
-frames.append(data)
-
-stream.stop_stream()
-stream.close()
-audio.terminate()
+p = pyaudio.PyAudio()
+for ii in range(p.get_device_count()):
+    print(p.get_device_info_by_index(ii).get('name'))
