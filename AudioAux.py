27c62abc152002a8bce1e55cd7b3537f88214bce
Guilherme Alvarenga Dias <guilhermeadias@tecnico.ulisboa.pt>
2022-12-04 02:46:13 +0000

    Button test

--- AudioAux.py
+++ AudioAux.py
@@ -2,6 +2,7 @@ import pyaudio
 import wave
 from gpiozero import LED, Button
 import time
+import os
 
 #GPIO
 BUT_1 = Button(27)
@@ -38,6 +39,15 @@ try:
             print("recording2")
             stream.stop_stream()
             stream.close()
+            wavefile = wave.open(wav_output_filename,'wb')
+            wavefile.setnchannels(chans)
+            wavefile.setsampwidth(audio.get_sample_size(form_1))
+            wavefile.setframerate(samp_rate)
+            wavefile.writeframes(b''.join(frames))
+            wavefile.close()
+
+            os.remove("test1.wav")
+
             while True:
                 if(BUT_1.is_active):
                     LED_G.off()
